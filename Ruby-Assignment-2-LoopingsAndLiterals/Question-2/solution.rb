60127d2b2c7f97cac031b59d9a47b326f4f57176
Shirish Parsekar <parsekarshirish171@gmail.com>
2016-09-14 16:06:41 +0530

    Consider first line.. Number of cases

--- Ruby-Assignment-2-LoopingsAndLiterals/Question-2/solution.rb
+++ Ruby-Assignment-2-LoopingsAndLiterals/Question-2/solution.rb
@@ -1,10 +1,13 @@
-require "byebug"
+
 input_file_name = "./input.txt"
 
 input_file = File.open(input_file_name)
 
 line_count = 0
 
+T = input_file.readline.chomp.to_i
+
+# method for checking common substring
 def hasCommonSubstring(string_A, string_B)
 	string_A.chomp.split('').each do |character|
 		return true if string_B.include? character
@@ -12,20 +15,17 @@ def hasCommonSubstring(string_A, string_B)
 	false
 end
 
-while !input_file.eof do
-	line_count += 1
+# the main flow of program
+iterator = 0
+while iterator < T && !input_file.eof? do
+	iterator += 1
 
 	line =  input_file.readline
-
-	# skip first line
-	next if line_count == 1
-
-	# first line of test case
+	# first string
 	string_A = line
 
 	line =  input_file.readline
-
-	# second line of test case
+	# second string
 	string_B = line
 
 	# check for common substring
