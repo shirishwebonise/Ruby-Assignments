aaee5c2907f60e805838e06dcee83b4cb104dacc
Shirish Parsekar <parsekarshirish@gmail.com>
2016-09-17 12:21:58 +0530

    Fix tab size to 2 spaces

--- Ruby-Assignment-2-LoopingsAndLiterals/Question-1/solution.rb
+++ Ruby-Assignment-2-LoopingsAndLiterals/Question-1/solution.rb
@@ -3,23 +3,23 @@ input_file_name = "./input.txt"
 
 # method for calculating sum of array elements
 def sumOfNumbers(int_array)
-	if int_array
-		total = int_array.reduce(:+)
-	end
-	total ? total : 0
+  if int_array
+    total = int_array.reduce(:+)
+  end
+  total ? total : 0
 end
 
 # method to check the array for the required condition
 def containsSuchElement(int_array)
-	int_array.each_with_index do |item, index|
-		leftArray = int_array[0, index]
-		rightArray = int_array[index+1, int_array.length]
+  int_array.each_with_index do |item, index|
+    leftArray = int_array[0, index]
+    rightArray = int_array[index+1, int_array.length]
 
-		if sumOfNumbers(leftArray) == sumOfNumbers(rightArray)
-			return true	
-		end
-	end
-	false
+    if sumOfNumbers(leftArray) == sumOfNumbers(rightArray)
+      return true	
+  	end
+  end
+  false
 end
 
 # read file
@@ -30,18 +30,18 @@ total_cases = input_file.readline.chomp.to_i
 
 # the main flow of program
 total_cases.times do
-	# read a line - number of elements in the array
-	totalElements =  input_file.readline.chomp.to_i
+  # read a line - number of elements in the array
+  totalElements =  input_file.readline.chomp.to_i
 
-	# second line - the array elements
-	# read the line, split, convert string elements into integers
-	line = input_file.readline.chomp
-	int_array = line.split.map{ |num| num.to_i }
+  # second line - the array elements
+  # read the line, split, convert string elements into integers
+  line = input_file.readline.chomp
+  int_array = line.split.map{ |num| num.to_i }
 
-	# check the array for given conditions
-	if( containsSuchElement(int_array) )
-		puts "YES"
-	else
-		puts "NO"
-	end
+  # check the array for given conditions
+  if( containsSuchElement(int_array) )
+    puts "YES"
+  else
+    puts "NO"
+  end
 end
