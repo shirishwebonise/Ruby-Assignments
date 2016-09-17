6a42678557dd66a40c4b3344f9b5648dbb408780
Shirish Parsekar <parsekarshirish@gmail.com>
2016-09-17 12:19:32 +0530

    Fix tab size to 2 spaces

--- Ruby-Assignment-2-LoopingsAndLiterals/Question-3/solution.rb
+++ Ruby-Assignment-2-LoopingsAndLiterals/Question-3/solution.rb
@@ -4,6 +4,6 @@ input_text = gets.chomp
 puts input_text
 
 while input_text.length>1 do
-	input_text.slice! input_text.length-2
-	puts input_text
+  input_text.slice! input_text.length-2
+  puts input_text
 end
