c9d5c044bdfae1b651b88feeb86f743ea446eb21
Shirish Parsekar <parsekarshirish171@gmail.com>
2016-09-20 13:32:47 +0530

    Rename Queue to QueueList

---
+++ Ruby-Assignment-5-gems/collection_manager/lib/queue_list.rb
@@ -0,0 +1,27 @@
+require_relative 'list'
+
+##
+# QueueList class implementing a typical Queue data structure
+
+class QueueList < List
+  def initialize(size = nil)
+    super(size)
+  end
+
+  ## 
+  # This method implements remove functionality of a Queue (FIFO)
+  #
+  # Example:
+  #   >> s = QueueList.new
+  #   >> s.push("else")
+  #   >> s.push("something")
+  #   >> s.remove()
+  # => "else"
+  def remove()
+    if(@list.length == 0)
+      puts "Queue empty!"
+    else
+      @list.shift
+    end
+  end
+end
