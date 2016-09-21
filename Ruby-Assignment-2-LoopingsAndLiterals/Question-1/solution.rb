59aeaa1bf5959efc133867639fc093f3de579c1a
Shirish Parsekar <parsekarshirish171@gmail.com>
2016-09-21 15:20:28 +0530

    Consider impovements  - map block  - guard condition  - shorten reduce statement for summing numbers in array  - remove sum_of_numbers function and use reduce statement inline instead

--- Ruby-Assignment-2-LoopingsAndLiterals/Question-1/solution.rb
+++ Ruby-Assignment-2-LoopingsAndLiterals/Question-1/solution.rb
@@ -1,21 +1,13 @@
 
 INPUT_FILE_NAME = "./input.txt"
 
-# method for calculating sum of array elements
-def sum_of_numbers(int_array)
-  total = int_array.reduce(:+)
-  total ? total : 0
-end
-
 # method to check the array for the required condition
 def array_has_such_element(int_array)
   int_array.each_with_index do |item, index|
     left_subarray = int_array[0, index]
     right_subarray = int_array[index+1, int_array.length]
 
-    if sum_of_numbers(left_subarray) == sum_of_numbers(right_subarray)
-      return true	
-    end
+    return true if left_subarray.reduce(0, :+) == right_subarray.reduce(0, :+)
   end
   false
 end
@@ -33,7 +25,7 @@ TOTAL_CASES.times do
 
   # second line - the array elements
   # read the line, split, convert string elements into integers
-  int_array = input_file.readline.chomp.split.map{ |num| num.to_i }
+  int_array = input_file.readline.chomp.split.map(&:to_i)
 
   # check the array for given conditions
   if( array_has_such_element(int_array) )
