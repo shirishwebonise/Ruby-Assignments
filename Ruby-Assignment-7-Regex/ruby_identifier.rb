819066dc65daae7441e1230328111cb5206a50fd
Shirish Parsekar <parsekarshirish171@gmail.com>
2016-09-23 13:13:45 +0530

    Fix class match, Add module match

--- Ruby-Assignment-7-Regex/ruby_identifier.rb
+++ Ruby-Assignment-7-Regex/ruby_identifier.rb
@@ -6,7 +6,7 @@ class RubyIdentifier
     
     file = open_file(file_path)
  
-    file_name = match_file_name_pattern(file_path)
+    file_name = Helpers::match_file_name_pattern(file_path)
 
     check_file_name(file_name)
 
@@ -24,19 +24,26 @@ class RubyIdentifier
 
     def check_file_name(file_name)
       if Helpers::match_file_name_pattern(file_name)
-        puts "Looks like a ruby file from the file name"
+        puts "Might be a ruby file as per the file name"
       else
         puts "That's not a ruby file"
       end
     end
 
     def check_class(file_name, file)
-      expected_class_name = snake_to_camel_case(file_name)
-
+      # expected_class_name = snake_to_camel_case(file_name)
       file.each do |line|
-        
+        match = $RUBY_CLASS_PATTERN.match(line)
+        puts "found a class: "+match.to_s if match
+
+        match = $RUBY_MODULE_PATTERN.match(line)
+        puts "found a module: "+match.to_s if match
       end
     end
 end
 
-RubyIdentifier.new.identify('./code_snippet.rb')
+Dir["./*"].each do |file_name| 
+  puts "checking file: " + file_name
+  RubyIdentifier.new.identify(file_name)
+  puts
+end
