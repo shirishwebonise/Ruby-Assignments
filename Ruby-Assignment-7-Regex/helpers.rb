aa268a93bc2b50df62a22ee16a5a66dde1038845
Shirish Parsekar <parsekarshirish171@gmail.com>
2016-09-23 13:12:52 +0530

    Fix class pattern, Add module pattern

--- Ruby-Assignment-7-Regex/helpers.rb
+++ Ruby-Assignment-7-Regex/helpers.rb
@@ -1,12 +1,14 @@
 
 module Helpers
   $RUBY_FILE_PATTERN = /([a-z0-9])+_?([a-z0-9])+\.rb$/
+  $RUBY_MODULE_PATTERN = /(?<=module )(\w)+\b/
+  $RUBY_CLASS_PATTERN = /(?<=class )(\w)+\b/
 
-  def snake_to_camel_case(file_name)
-
+  def self.snake_to_camel_case(file_name)
+    # code to change case
   end
 
-  def match_file_name_pattern(file_name)
-    $RUBY_FILE_PATTERN.match(file_name)
+  def self.match_file_name_pattern(file_name)
+    $RUBY_FILE_PATTERN.match(file_name.to_s)
   end
 end
