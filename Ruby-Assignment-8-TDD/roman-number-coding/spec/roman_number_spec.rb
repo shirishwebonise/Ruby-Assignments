44da3dd408be80b626c605869ee29bed1a9c1dfc
Shirish Parsekar <parsekarshirish171@gmail.com>
2016-09-27 16:38:50 +0530

    Add specification for negative flow

--- Ruby-Assignment-8-TDD/roman-number-coding/spec/roman_number_spec.rb
+++ Ruby-Assignment-8-TDD/roman-number-coding/spec/roman_number_spec.rb
@@ -8,57 +8,56 @@ RSpec.describe RomanNumber do
 
     it "converts single roman number I" do
       roman_number = create_roman_number 'I'
-      expect(roman_number.to_decimal).to eq 1
+      expect(roman_number.decimal_value).to eq 1
     end
 
     it "converts single roman number V" do
       roman_number = create_roman_number 'V'
-      expect(roman_number.to_decimal).to eq 5
+      expect(roman_number.decimal_value).to eq 5
     end
 
     it "converts single roman number X" do
       roman_number = create_roman_number 'X'
-      expect(roman_number.to_decimal).to eq 10
+      expect(roman_number.decimal_value).to eq 10
     end
 
     it "converts single roman number L" do
       roman_number = create_roman_number 'L'
-      expect(roman_number.to_decimal).to eq 50
+      expect(roman_number.decimal_value).to eq 50
     end
 
     it "converts single roman number C" do
       roman_number = create_roman_number 'C'
-      expect(roman_number.to_decimal).to eq 100
+      expect(roman_number.decimal_value).to eq 100
     end
 
     it "converts single roman number D" do
       roman_number = create_roman_number 'D'
-      expect(roman_number.to_decimal).to eq 500
+      expect(roman_number.decimal_value).to eq 500
     end
 
     it "converts single roman number M" do
       roman_number = create_roman_number 'M'
-      expect(roman_number.to_decimal).to eq 1000
+      expect(roman_number.decimal_value).to eq 1000
     end
 
     it "converts roman number with type: first less than second" do
       roman_number = create_roman_number 'IV'
-      expect(roman_number.to_decimal).to eq 4
+      expect(roman_number.decimal_value).to eq 4
     end
 
     it "converts roman number with type: first more than second" do
       roman_number = create_roman_number 'XI'
-      expect(roman_number.to_decimal).to eq 11
+      expect(roman_number.decimal_value).to eq 11
     end
 
     it "converts a large roman number" do
       roman_number = create_roman_number 'MCMLXXXIV'
-      expect(roman_number.to_decimal).to eq 1984
+      expect(roman_number.decimal_value).to eq 1984
     end
 
-    it "returns 0 for invalid representation" do
-      roman_number = create_roman_number 'HJK'
-      expect(roman_number.to_decimal).to eq 0
+    it "raises exception for invalid representation" do
+      expect{ create_roman_number 'HXI' }.to raise_error(ArgumentError)
     end
   end
 end
