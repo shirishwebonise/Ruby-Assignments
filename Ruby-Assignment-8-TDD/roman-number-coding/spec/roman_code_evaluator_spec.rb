71516da665770b106fb490387dda27d2efa7c21f
Shirish Parsekar <parsekarshirish171@gmail.com>
2016-09-27 16:38:28 +0530

    Refactor

--- Ruby-Assignment-8-TDD/roman-number-coding/spec/roman_code_evaluator_spec.rb
+++ Ruby-Assignment-8-TDD/roman-number-coding/spec/roman_code_evaluator_spec.rb
@@ -3,17 +3,15 @@ require 'roman_code_evaluator'
 RSpec.describe RomanCodeEvaluator do
   let(:code_evaluator) { RomanCodeEvaluator.new }
 
-  context "when RomanCodeEvaluator is instantiated" do
+  context "after RomanCodeEvaluator is instantiated" do
     it "evaluates multiplicatives" do
-      expect(code_evaluator.multiplicative("Silver")).to eq 17
-      expect(code_evaluator.multiplicative("Gold")).to eq 14450
-      expect(code_evaluator.multiplicative("Iron")).to eq 195.5
+      expect(code_evaluator.eval_multiplicative("coin coin Silver 34")).to eq 17
+      expect(code_evaluator.eval_multiplicative("coin dollar Gold 57800")).to eq 14450
+      expect(code_evaluator.eval_multiplicative("yen yen Iron 3910")).to eq 195.5
     end
-  end
 
-  context "when code is evaluated" do
-    it "gives result of evaluated code" do
-      result = code_evaluator.evaluate 'yen rupee coin coin'
+    it "evaluates expression" do
+      result = code_evaluator.eval_exp 'yen rupee coin coin'
       expect(result).to eq 42
     end
   end
