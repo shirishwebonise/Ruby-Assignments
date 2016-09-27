999894c57e08bd476171249b1c04520cc01afc6a
Shirish Parsekar <parsekarshirish171@gmail.com>
2016-09-27 16:34:51 +0530

    Refactor to clean the code  - rename mappings to MAPPINGS to make it constant  - raise error if input is not in proper roman form  - rename method to_decimal to decimal_value  - extract method from implementation of decimal_value

--- Ruby-Assignment-8-TDD/roman-number-coding/lib/roman_number.rb
+++ Ruby-Assignment-8-TDD/roman-number-coding/lib/roman_number.rb
@@ -1,33 +1,36 @@
 
 class RomanNumber
-  @@mappings = { M: 1000, D: 500, C: 100, L: 50, X:10, V: 5, I: 1 }
+  @@MAPPINGS = { M: 1000, D: 500, C: 100, L: 50, X:10, V: 5, I: 1 }
   attr_reader :number_str
   alias_method :as_string, :number_str
 
   def initialize(number_str)
+    raise ArgumentError, "Invalid Roman Number" if /[^MDCLXVI]/.match(number_str)
     @number_str = number_str
   end
 
-  def to_decimal
-    converted_num = 0
-    last_decimal = 0
-    @number_str.split('').reverse.each do |element|
-      element_decimal = decimal_value(element)
-      return 0 if element_decimal.nil?
-
-      if( last_decimal > element_decimal)
-        converted_num -= element_decimal
-      else
-        converted_num += element_decimal
-      end
-      last_decimal = element_decimal
-    end
-    converted_num
+  def decimal_value
+    evaluate_to_decimal
   end
 
   private
-    def decimal_value(roman)
+    def decimal(roman)
       return 0 if roman.nil?
-      @@mappings[roman.to_sym]
+      @@MAPPINGS[roman.to_sym]
+    end
+
+    def evaluate_to_decimal
+      converted_num = 0
+      last_decimal = 0
+      @number_str.split('').reverse.each do |element|
+        element_decimal = decimal(element)
+        if( last_decimal > element_decimal)
+          converted_num -= element_decimal
+        else
+          converted_num += element_decimal
+        end
+        last_decimal = element_decimal
+      end
+      converted_num
     end
 end
