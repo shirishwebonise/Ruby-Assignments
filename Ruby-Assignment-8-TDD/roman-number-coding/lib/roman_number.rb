925ec0fbb790ba0e6d6d40115d88359d57875676
Shirish Parsekar <parsekarshirish171@gmail.com>
2016-09-27 11:43:09 +0530

    Remove roman-numeral gem

--- Ruby-Assignment-8-TDD/roman-number-coding/lib/roman_number.rb
+++ Ruby-Assignment-8-TDD/roman-number-coding/lib/roman_number.rb
@@ -1,6 +1,7 @@
 require 'roman-numerals'
 
 class RomanNumber
+  @@mappings = { M: 1000, D: 500, C: 100, L: 50, X:10, V: 5, I: 1 }
   attr_reader :number_str
   alias_method :as_string, :number_str
 
@@ -9,6 +10,25 @@ class RomanNumber
   end
 
   def to_decimal
-    RomanNumerals.to_decimal(@number_str)
+    converted_num = 0
+    last_decimal = 0
+    @number_str.split('').reverse.each do |element|
+      element_decimal = decimal_value(element)
+      return 0 if element_decimal.nil?
+
+      if( last_decimal > element_decimal)
+        converted_num -= element_decimal
+      else
+        converted_num += element_decimal
+      end
+      last_decimal = element_decimal
+    end
+    converted_num
   end
+
+  private
+    def decimal_value(roman)
+      return 0 if roman.nil?
+      @@mappings[roman.to_sym]
+    end
 end
